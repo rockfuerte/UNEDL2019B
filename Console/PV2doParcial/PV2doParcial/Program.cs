361328fb99ccae1e3fb068895863ce6fb256c873
rockfuerte <36205721+rockfuerte@users.noreply.github.com>
2019-11-27 07:41:48 -0600

    Add files via upload

---
+++ Console/PV2doParcial/PV2doParcial/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PV2doParcial
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack(); ;
+            String v;
+            Console.WriteLine("Ingresa paréntesis: ");
+            v = Console.ReadLine();
+            foreach (char i in v)
+            {
+                if (i=='['||i=='{'||i=='(')
+                {
+                    stack.Push(i);
+                }
+                else if (stack.Count>0)
+                {
+                    if (i==')')
+                    {
+                        stack.Pop();
+                    }
+                    else if (i==']')
+                    {
+                        stack.Pop();
+                    }
+                    else if (i=='}')
+                    {
+                        stack.Pop();
+                    }
+                }
+            }
+            if (stack.Count >= 1)
+            {
+                Console.Write("Ecuación no  balanceada");
+            }
+            else
+            {
+                Console.Write("Ecuación balanceada");
+            }
+        }
+    }
+}
