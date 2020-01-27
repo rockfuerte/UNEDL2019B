df1f4f3d331b814686637874c293d4dff3131d7b
rockfuerte <36205721+rockfuerte@users.noreply.github.com>
2020-01-27 10:46:29 -0600

    Add files via upload

---
+++ Windows/TPV2Parcial/ExamenTotalizador/ExamenTotalizador/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ExamenTotalizador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = {
+                6, 4, 1
+            };
+            int t;
+            int n = 0;
+            for (int j = 0; j <= a.Length - 2; j++)
+            {
+                for (int i = 0; i <= a.Length - 2; i++)
+                {
+                    if (a[i] > a[i + 1])
+                    {
+                        t = a[i + 1];
+                        a[i + 1] = a[i];
+                        a[i] = t;
+                        n++;
+                    }
+                }
+            }
+            Console.WriteLine("Orden nuevo:");
+            Console.Write("[");
+            foreach (int p in a)
+            {
+                Console.Write(p + ", ");
+            }
+            Console.Write("]");
+            Console.Write("\nPrimer elemento: " + a[0]);
+            Console.Write("\nUltimo elemento: " + a[2]);
+            Console.Write("\nLas permutas son: " + n);
+        } 
+
+    }
+}
